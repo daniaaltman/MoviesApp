0f2885f9138ba965981727ccb637d5f035c77770
daniaaltman <76812419+daniaaltman@users.noreply.github.com>
2022-04-07 14:56:11 +0300

    add switch

--- collectionView/MovieRequest.swift
+++ collectionView/MovieRequest.swift
@@ -6,33 +6,56 @@
 //
 
 import Foundation
+
 class MovieRequest {
     
+    var currdata: Data?
+    var movieData: [MovieData] = []
     
-    
-    func congifure() {
-        httpRequest()
+    enum requestType {
+        case popularity
+        case highestRated
+        case popularKids
+        
+        
     }
-    func httpRequest() {
+    
+    func selectType (type: requestType) -> String {
+        switch type {
+        case .popularity:
+           return "discover/movie?sort_by=popularity.desc&"
+        case .highestRated:
+           return "/discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc&"
+        case .popularKids:
+           return "/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&"
+        }
+    }
+    
+    
+    func httpRequest(_ type: requestType) {
         let token = "api_key=0cfd047bf8d2487c8c1b8e5a95ca5afd"
         let maniUrl = "https://api.themoviedb.org/3/"
-        let popularity = "discover/movie?sort_by=popularity.desc&"
-        let url = URL(string: "\(maniUrl)\(popularity)\(token)")!
+        let selectedType = selectType(type: type)
+        let url = URL(string: "\(maniUrl)\(selectedType)\(token)")!
 
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard
                 error == nil,
                 let data = data,
                 let string = String(data: data, encoding: .utf8)
+                    
             else {
                 print(error ?? "Unknown error")
                 return
             }
+            self.currdata = data
 
             print(string.data(using: .utf8)?.prettyPrintedJSONString ?? "no")
         }
         task.resume()
     }
+    
+    
 }
 
 extension Data {
