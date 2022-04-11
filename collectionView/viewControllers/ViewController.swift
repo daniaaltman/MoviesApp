f5fee8fc942030cb57ada668c4fffc9635c9e821
daniaaltman <sbhvjfnv@gmail.com>
2022-04-11 18:58:55 +0300

    added release date

--- collectionView/viewControllers/ViewController.swift
+++ collectionView/viewControllers/ViewController.swift
@@ -6,23 +6,16 @@
 //
 
 import UIKit
-
+	
 class ViewController: UIViewController, UISearchBarDelegate, UISearchDisplayDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
-<<<<<<< HEAD:collectionView/ViewController.swift
-    let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
-    let itemsPerRow:CGFloat = 3
-    lazy var request: MovieRequest = MovieRequest()
-    
-=======
     @IBOutlet weak var searchBar: UISearchBar!
     let sectionInsets = UIEdgeInsets(top: 10.0, left: 20.0, bottom: 50.0, right: 20.0)
     let itemsPerRow:CGFloat = 2
     lazy var movieApi: MovieApi = MovieApi()
     var moviesArray: [MovieData] = []
     var originalMoviesArray: [MovieData] = []
->>>>>>> new-branch:collectionView/viewControllers/ViewController.swift
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,15 +45,6 @@ class ViewController: UIViewController, UISearchBarDelegate, UISearchDisplayDele
             collectionView.dataSource = self
         }
         
-<<<<<<< HEAD:collectionView/ViewController.swift
-        collectionView.collectionViewLayout = layout
-        collectionView.delegate = self
-        collectionView.dataSource = self
-
-
-        request.httpRequest(.popularity)
-=======
->>>>>>> new-branch:collectionView/viewControllers/ViewController.swift
     }
     
     func fetchAndPlaceData() async {
